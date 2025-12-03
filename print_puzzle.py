ca33a8e89811ddc28d8295b461bae1db65ddb6e4
Nathaniel Carlson <157933901+Ncarlson27@users.noreply.github.com>
2025-12-03 11:41:32 -0700

    Add files via upload

--- print_puzzle.py
+++ print_puzzle.py
@@ -1,4 +1,4 @@
-def print_board(board: list, n: int, file_name):
+def print_board(board: list, n: int = 3, file_name = None):
     """
     Params:
         board: 2-d list representing the board
@@ -8,25 +8,42 @@ def print_board(board: list, n: int, file_name):
     Returns:
         nothing
 
-    Writes the Sudoku board to a txt file in an easy to read way, shows all boxes
+    Writes the Sudoku board to a txt file (if file name is given) in an easy to read way, shows all boxes
+    If no file name given, prints to the terminal
     """
+    if file_name is not None:
+        with open(file_name, 'a') as file:
+            file.write("Puzzle:\n")
+            for i in range(len(board)):
+                if i % n == 0 and i != 0:
+                    file.write("- " * ((n**2) + 1))
+                    file.write("\n")
 
-    with open(file_name, 'a') as file:
-        file.write("Puzzle:\n")
+                for j in range(len(board[0])):
+                    if j % n == 0 and j != 0:
+                        file.write("|")
+                    
+                    if j == (n**2) - 1:
+                        file.write(f"{board[i][j]}")
+                        file.write("\n")
+
+                    else:
+                        file.write(f"{board[i][j]} ")
+            
+            file.write("\n\n")
+
+    else:
+        print("Puzzle:\n")
         for i in range(len(board)):
             if i % n == 0 and i != 0:
-                file.write("- " * ((n**2) + 1))
-                file.write("\n")
+                print("- " * ((n**2) + 1))
 
             for j in range(len(board[0])):
                 if j % n == 0 and j != 0:
-                    file.write("|")
+                    print("|", end="")
                 
                 if j == (n**2) - 1:
-                    file.write(f"{board[i][j]}")
-                    file.write("\n")
+                    print(f"{board[i][j]}")
 
                 else:
-                    file.write(f"{board[i][j]} ")
-        
-        file.write("\n\n")
+                    print(f"{board[i][j]} ", end="")
